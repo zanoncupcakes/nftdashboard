eb676f7d9c9008d5eec397e31d6c82fa57b30017
zanoncupcakes <103525024+zanoncupcakes@users.noreply.github.com>
2022-04-15 14:50:57 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "BAYC",
 
     // 4. (Optional) Display the floor price chart
     "timeseries_chart": true,
 
     // 5. (Optional) Set your banner image
-    "banner_picture": "https://www.superflexfitness.com/wp-content/uploads/2017/03/3D-banner-background.jpg",
+    "banner_picture": "https://lh3.googleusercontent.com/pdSlqsRXIGC69bqiLX-TF760GkBVB0Z-pehDpYT7XDsPEPHOq0_o7dNXcmnkoEVomTOwgrgOzlBj89IGdc8ROCPkoAAGBnHt9R__HQ=w600",
   },
 
   // Supported networks - just for your reference
