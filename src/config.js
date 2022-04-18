9ee24333b75284394876d09e364d78623a09580d
zanoncupcakes <103525024+zanoncupcakes@users.noreply.github.com>
2022-04-18 10:06:00 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
+    "collection_address": "0x7b557aA52d0055d84b1E3f5487D9018f318372C1",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
     "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "BAYC",
+    "title": "Birbs",
 
     // 4. (Optional) Display the floor price chart
     "timeseries_chart": true,
 
     // 5. (Optional) Set your banner image
-    "banner_picture": "https://lh3.googleusercontent.com/pdSlqsRXIGC69bqiLX-TF760GkBVB0Z-pehDpYT7XDsPEPHOq0_o7dNXcmnkoEVomTOwgrgOzlBj89IGdc8ROCPkoAAGBnHt9R__HQ=w600",
+    "banner_picture": "https://lh3.googleusercontent.com/hint300g1bPSvzYS1Lf5IR0G5WlkUJJcjqQB7b3qRzjt5sUEHctqCIQfUkzy4hkxG1ev4zU4XdAkB-RwmsE2G_Ye8jhkyyfYMr5boQ=w282",
   },
 
   // Supported networks - just for your reference
